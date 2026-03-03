7252301b1f063815d4eec6ffe800ff15d186b9d3
Seir <seirdotexe@gmail.com>
2026-03-03 22:03:17 +0100

    feat: structure for serializing

--- src/AMF.js
+++ src/AMF.js
@@ -1,9 +1,41 @@
+import ClassAlias from './AMF/alias.js';
+
+import { default as Serializer0 } from './AMF0/serializer.js';
+import { default as Serializer3 } from './AMF3/serializer.js';
+
+/**
+ * @author SeirDotExe
+ * @license BSD-3-Clause
+ */
 export class AMF {
+  /**
+   * Initialize the class alias holder
+   * @static
+   * @type {ClassAlias}
+   */
+  static classAlias = new ClassAlias();
+
+  /**
+   * Initialize the serializers
+   * @static
+   * @type {{ 0: Serializer0, 3: Serializer3}}
+   */
+  static #serializers = {
+    0: new Serializer0(this.classAlias),
+    3: new Serializer3(this.classAlias)
+  };
+
+  /**
+   * Serialize an object into AMF binary data
+   * @static
+   * @param {any} value - Any supported value to serialize
+   * @param {0|3} [version=3] - The AMF version
+   * @returns {Buffer} Returns the AMF data in a buffer
+   */
   static serialize(value, version = 3) {
-
+    return this.#serializers[version].serialize(value).flush();
   }
 
-  static deserialize(buffer, version = 3) {
 
-  }
+  static deserialize(buffer, version = 3) { }
 }
---
+++ src/AMF0/serializer.js
@@ -0,0 +1,56 @@
+import DynBuffer from '@seirdotexe/dynbuffer';
+
+/**
+ * @typedef {import('../AMF/alias.js').default} ClassAlias
+ */
+
+/**
+ * @exports
+ * @default
+ * @class
+ * @module AMF0
+ */
+export default class Serializer {
+  /**
+   * The DynBuffer instance containing written AMF bytes from this instance
+   * @private
+   * @type {DynBuffer}
+   */
+  #dynbuf;
+  /**
+   * The general class alias holder
+   * @private
+   * @type {ClassAlias}
+   */
+  #classAlias;
+
+  /**
+   * Creates a new AMF0 serializer
+   * @param {ClassAlias} classAlias - The class alias internally coming from AMF entrypoint class
+   */
+  constructor(classAlias) {
+    this.#dynbuf = new DynBuffer();
+    this.#classAlias = classAlias;
+  }
+
+  /**
+   * Flushes the DynBuffer instance by saving the current stream, clearing it, and returning the stream containing AMF bytes
+   * @returns {Buffer} The buffer containing AMF bytes
+   */
+  flush() {
+    const { stream } = this.#dynbuf;
+
+    this.#dynbuf.clear();
+
+    return stream;
+  }
+
+  /**
+   * Serializes a value in AMF0 format
+   * @param {any} value - The value to serialize
+   * @returns {Serializer}
+   */
+  serialize(value) {
+    return this;
+  }
+}
