b78cd46dc20dd212dbdfe9756475f7ed30d86f27
Seir <seirdotexe@gmail.com>
2026-03-05 14:46:32 +0100

    Create markers.js

---
+++ src/AMF/markers.js
@@ -0,0 +1,92 @@
+/**
+ * @typedef {Object} AMF0Markers
+ * @property {number} NUMBER
+ * @property {number} BOOLEAN
+ * @property {number} STRING
+ * @property {number} OBJECT
+ * @property {number} MOVIECLIP
+ * @property {number} NULL
+ * @property {number} UNDEFINED
+ * @property {number} REFERENCE
+ * @property {number} ECMA_ARRAY
+ * @property {number} OBJECT_END
+ * @property {number} STRICT_ARRAY
+ * @property {number} DATE
+ * @property {number} LONG_STRING
+ * @property {number} UNSUPPORTED
+ * @property {number} RECORDSET
+ * @property {number} XML_DOCUMENT
+ * @property {number} TYPED_OBJECT
+ * @property {number} AVMPLUS
+ *
+ * @typedef {Object} AMF3Markers
+ * @property {number} UNDEFINED
+ * @property {number} NULL
+ * @property {number} FALSE
+ * @property {number} TRUE
+ * @property {number} INTEGER
+ * @property {number} DOUBLE
+ * @property {number} STRING
+ * @property {number} XML_DOCUMENT
+ * @property {number} DATE
+ * @property {number} ARRAY
+ * @property {number} OBJECT
+ * @property {number} XML
+ * @property {number} BYTE_ARRAY
+ * @property {number} VECTOR_INT
+ * @property {number} VECTOR_UINT
+ * @property {number} VECTOR_DOUBLE
+ * @property {number} VECTOR_OBJECT
+ * @property {number} DICTIONARY
+ *
+ * @typedef {Object} AMFMarkers
+ * @property {Readonly<AMF0Markers>} AMF0 - AMF0 type markers
+ * @property {Readonly<AMF3Markers>} AMF3 - AMF3 type markers
+ */
+
+/** @type {Readonly<AMFMarkers>} */
+const Markers = {
+  AMF0: {
+    NUMBER: 0x00,
+    BOOLEAN: 0x01,
+    STRING: 0x02,
+    OBJECT: 0x03,
+    MOVIECLIP: 0x04,
+    NULL: 0x05,
+    UNDEFINED: 0x06,
+    REFERENCE: 0x07,
+    ECMA_ARRAY: 0x08,
+    OBJECT_END: 0x09,
+    STRICT_ARRAY: 0x0A,
+    DATE: 0x0B,
+    LONG_STRING: 0x0C,
+    UNSUPPORTED: 0x0D,
+    RECORDSET: 0x0E,
+    XML_DOCUMENT: 0x0F,
+    TYPED_OBJECT: 0x10,
+    AVMPLUS: 0x11
+  },
+
+  AMF3: {
+    UNDEFINED: 0x00,
+    NULL: 0x01,
+    FALSE: 0x02,
+    TRUE: 0x03,
+    INTEGER: 0x04,
+    DOUBLE: 0x05,
+    STRING: 0x06,
+    XML_DOCUMENT: 0x07,
+    DATE: 0x08,
+    ARRAY: 0x09,
+    OBJECT: 0x0A,
+    XML: 0x0B,
+    BYTE_ARRAY: 0x0C,
+    VECTOR_INT: 0x0D,
+    VECTOR_UINT: 0x0E,
+    VECTOR_DOUBLE: 0x0F,
+    VECTOR_OBJECT: 0x10,
+    DICTIONARY: 0x11
+  }
+};
+
+export default Object.freeze(Markers);
