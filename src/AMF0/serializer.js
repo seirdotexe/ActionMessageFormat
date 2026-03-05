ae833d0252d3fc594ce4d0e66f2d206642f1aa8d
Seir <seirdotexe@gmail.com>
2026-03-05 14:47:07 +0100

    feat: AMF0 serializer skeleton

--- src/AMF0/serializer.js
+++ src/AMF0/serializer.js
@@ -1,4 +1,6 @@
 import DynBuffer from '@seirdotexe/dynbuffer';
+import { isBoxedPrimitive } from 'node:util/types';
+import Markers from '../AMF/markers.js';
 
 /**
  * @typedef {import('../AMF/alias.js').default} ClassAlias
@@ -51,6 +53,120 @@ export default class Serializer {
    * @returns {Serializer}
    */
   serialize(value) {
+    if (isBoxedPrimitive(value)) {
+      return this.serialize(value.valueOf());
+    } else if (value === null) {
+      this.#serializeNull();
+    } else if (value === undefined) {
+      this.#serializeUndefined();
+    } else {
+      const type = value?.constructor?.name;
+
+      switch (type) {
+        case 'Number': this.#serializeNumber(value); break;
+        case 'Boolean': this.#serializeBoolean(value); break;
+        case 'String': this.#serializeString(value); break;
+        case 'Object': this.#serializeObject(value); break;
+        case 'Array': this.#serializeArray(value); break;
+        case 'Date': this.#serializeDate(value); break;
+        default: this.#serializeUnidentifiedObject(value);
+      }
+    }
+
     return this;
+  }
+
+  /**
+   * Serializes a null
+   * @private
+   */
+  #serializeNull() {
+    this.#dynbuf.writeByte(Markers.AMF0.NULL);
+  }
+
+  /**
+   * Serializes an undefined
+   * @private
+   */
+  #serializeUndefined() {
+    this.#dynbuf.writeByte(Markers.AMF0.UNDEFINED);
+  }
+
+  /**
+   * Serializes a number
+   * @private
+   * @param {number} value - The number to serialize
+   */
+  #serializeNumber(value) {
+    this.#dynbuf.writeByte(Markers.AMF0.NUMBER);
+    this.#dynbuf.writeDouble(value);
+  }
+
+  /**
+   * Serializes a boolean
+   * @private
+   * @param {boolean} value - The boolean to serialize
+   */
+  #serializeBoolean(value) {
+    this.#dynbuf.writeByte(Markers.AMF0.BOOLEAN);
+    this.#dynbuf.writeBoolean(value);
+  }
+
+  /**
+   * Serializes a (long) string
+   * @private
+   * @param {string} value - The string to serialize
+   */
+  #serializeString(value) { }
+
+  /**
+   * Serializes an object
+   * @private
+   * @param {object} value - The object to serialize
+   */
+  #serializeObject(value) { }
+
+  /**
+   * Serializes an array
+   * @private
+   * @param {any[]} value - The array to serialize
+   */
+  #serializeArray(value) { }
+
+  /**
+   * Serializes a date
+   * @private
+   * @param {Date} value - The date to serialize
+   */
+  #serializeDate(value) { }
+
+  /**
+   * Serializes an unidentified object
+   * @private
+   * @param {any} value - The unidentified object to serialize
+   */
+  #serializeUnidentifiedObject(value) { }
+
+  /**
+   * Serializes a typed object
+   * @private
+   * @param {any} value - The typed object to serialize
+   * @param {string} aliasName - The alias name of the typed object
+   */
+  #serializeTypedObject(value, aliasName) { }
+
+  /**
+   * Serializes a reference
+   * @private
+   * @param {number} index - The index of the referenced value to serialize
+   */
+  #serializeReference(index) { }
+
+  /**
+   * Serializes an unsupported value
+   * @private
+   */
+  #serializeUnsupported() {
+    this.#dynbuf.writeByte(Markers.AMF0.UNSUPPORTED);
   }
 }
