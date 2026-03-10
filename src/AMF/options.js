df2e02dde7a340fd7ffb82dd493b62aa7bae5e00
Seir <seirdotexe@gmail.com>
2026-03-10 19:14:45 +0100

    Update options.js

--- src/AMF/options.js
+++ src/AMF/options.js
@@ -2,8 +2,8 @@
  * @module AMF/Options
  *
  * @typedef {object} AMFOptions
- * @property {boolean} compressSparse - Whether or not to compress AMF0 sparse array entries. Default to true
- * @property {boolean} dateOffset - Whether or not to utilize AMF0 date offset. Default to false
+ * @property {boolean} compressSparse - Whether or not to compress **AMF0** sparse array entries. Default to true
+ * @property {boolean} dateOffset - Whether or not to utilize **AMF0** date offset. Default to false
  */
 
 /** @type {AMFOptions} */
