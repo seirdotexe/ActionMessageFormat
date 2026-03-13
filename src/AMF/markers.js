f51164b8eadeec382d286be1f6684cbb841ba01b
Seir <seirdotexe@gmail.com>
2026-03-14 00:03:42 +0100

    chore: updated jsdoc for markers

--- src/AMF/markers.js
+++ src/AMF/markers.js
@@ -2,44 +2,44 @@
  * @module AMF/Markers
  *
  * @typedef {object} AMF0Markers
- * @property {number} NUMBER
- * @property {number} BOOLEAN
- * @property {number} STRING
- * @property {number} OBJECT
- * @property {number} MAP
- * @property {number} NULL
- * @property {number} UNDEFINED
- * @property {number} REFERENCE
- * @property {number} ECMA_ARRAY
- * @property {number} OBJECT_END
- * @property {number} STRICT_ARRAY
- * @property {number} DATE
- * @property {number} LONG_STRING
- * @property {number} UNSUPPORTED
- * @property {number} SET
- * @property {number} XML_DOCUMENT
- * @property {number} TYPED_OBJECT
- * @property {number} AVMPLUS
+ * @property {number} NUMBER 0x00
+ * @property {number} BOOLEAN 0x01
+ * @property {number} STRING 0x02
+ * @property {number} OBJECT 0x03
+ * @property {number} MAP 0x04 -- MOVIECLIP
+ * @property {number} NULL 0x05
+ * @property {number} UNDEFINED 0x06
+ * @property {number} REFERENCE 0x07
+ * @property {number} ECMA_ARRAY 0x08
+ * @property {number} OBJECT_END 0x09
+ * @property {number} STRICT_ARRAY 0x0A
+ * @property {number} DATE 0x0B
+ * @property {number} LONG_STRING 0x0C
+ * @property {number} UNSUPPORTED 0x0D
+ * @property {number} SET 0x0E -- RECORDSET
+ * @property {number} XML_DOCUMENT 0x0F
+ * @property {number} TYPED_OBJECT 0x10
+ * @property {number} AVMPLUS 0x11
  *
  * @typedef {object} AMF3Markers
- * @property {number} UNDEFINED
- * @property {number} NULL
- * @property {number} FALSE
- * @property {number} TRUE
- * @property {number} INTEGER
- * @property {number} DOUBLE
- * @property {number} STRING
- * @property {number} XML_DOCUMENT
- * @property {number} DATE
- * @property {number} ARRAY
- * @property {number} OBJECT
- * @property {number} XML
- * @property {number} BYTE_ARRAY
- * @property {number} VECTOR_INT
- * @property {number} VECTOR_UINT
- * @property {number} VECTOR_DOUBLE
- * @property {number} VECTOR_OBJECT
- * @property {number} DICTIONARY
+ * @property {number} UNDEFINED 0x00
+ * @property {number} NULL 0x01
+ * @property {number} FALSE 0x02
+ * @property {number} TRUE 0x03
+ * @property {number} INTEGER 0x04
+ * @property {number} DOUBLE 0x05
+ * @property {number} STRING 0x06
+ * @property {number} XML_DOCUMENT 0x07
+ * @property {number} DATE 0x08
+ * @property {number} ARRAY 0x09
+ * @property {number} OBJECT 0x0A
+ * @property {number} XML 0x0B
+ * @property {number} BYTE_ARRAY 0x0C
+ * @property {number} VECTOR_INT 0x0D
+ * @property {number} VECTOR_UINT 0x0E
+ * @property {number} VECTOR_DOUBLE 0x0F
+ * @property {number} VECTOR_OBJECT 0x10
+ * @property {number} DICTIONARY 0x11
  *
  * @typedef {object} AMFMarkers
  * @property {Readonly<AMF0Markers>} AMF0
@@ -64,7 +64,7 @@ const Markers = {
     LONG_STRING: 0x0C,
     UNSUPPORTED: 0x0D,
     SET: 0x0E, //! Off-spec - Replace unused 'RECORDSET' with 'SET'
-    XML_DOCUMENT: 0x0F,
+    XML_DOCUMENT: 0x0F, // Todo - Convert to string?
     TYPED_OBJECT: 0x10,
     AVMPLUS: 0x11
   },
@@ -77,11 +77,11 @@ const Markers = {
     INTEGER: 0x04,
     DOUBLE: 0x05,
     STRING: 0x06,
-    XML_DOCUMENT: 0x07,
+    XML_DOCUMENT: 0x07, // Todo - Convert to string?
     DATE: 0x08,
     ARRAY: 0x09,
     OBJECT: 0x0A,
-    XML: 0x0B,
+    XML: 0x0B, // Todo - Convert to string?
     BYTE_ARRAY: 0x0C,
     VECTOR_INT: 0x0D,
     VECTOR_UINT: 0x0E,
