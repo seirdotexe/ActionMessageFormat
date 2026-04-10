1036d2892b07472bb7cc394cc68e83c2e0f28492
Seir <seirdotexe@gmail.com>
2026-04-10 20:07:29 +0200

    chore: clean code

--- test/http.js
+++ test/http.js
@@ -4,16 +4,8 @@ const toHex = (buffer) => buffer.toString('hex').match(/.{1,2}/g)?.join(' ');
 const app = express();
 const port = 3000;
 
-app.use('/gateway', express.raw({
-  type: 'application/x-amf'
-}));
-
+app.use('/gateway', express.raw({ type: 'application/x-amf' }));
 app.get('/gateway', (req, res) => res.send('Hello, world!'));
-
-app.post('/gateway', (req, res) => {
-  const buffer = req.body;
-
-  console.log(toHex(buffer));
-});
+app.post('/gateway', (req, res) => console.log(toHex(req.body)));
 
 app.listen(port, () => console.log(`AMF binary gateway listening on http://localhost:${port}/gateway.`));
