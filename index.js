cc909edd653262f8346d9d784a76b73f96b0178b
Seir <seirdotexe@gmail.com>
2026-04-30 21:17:34 +0200

    fix: export Header and Message for types

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { AMF as default } from './src/AMF.js';
+export { default as Header } from './src/AMF/remoting/Header.js';
+export { default as Message } from './src/AMF/remoting/Message.js';
 export { default as Packet } from './src/AMF/remoting/packet.js';
